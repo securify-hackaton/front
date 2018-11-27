831bbf9fd46c6b7441ef4afaf976f03dd3753fd0
Guillaume BAECHLER <baechler.guillaume.pro@gmail.com>
2018-11-27 14:47:40 +0100

    adding a magnifaïk background

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on my app!</Text>
+        <LinearGradient
+          colors={['#3D393A', '#10101D']}
+          style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0
+          }}
+          start={{x: 0, y: 0}}
+          end={{x: 1, y: 1}}
+        />
       </View>
     );
   }
